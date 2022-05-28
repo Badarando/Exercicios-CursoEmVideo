cf94a4fce382218edbb0b2396277773607d7b021
Marcos Paulo Badaró <marcospaulobadaro@gmail.com>
2022-05-27 23:47:27 -0300

    fazendo exercícios

---
+++ ex11/script.js
@@ -0,0 +1,18 @@
+
+// Minha resolução
+
+// let nota1 = Number( window.prompt('Digite a primeira nota: '));
+// let nota2 = Number(window.prompt('Digite a segunda nota: '));
+// let nota3 = Number(window.prompt('Digite a terceira nota: '));
+// let nota4 = Number(window.prompt('Digite a quarta nota: ')); 
+
+
+// let total = 0;
+// total = (nota1 + nota2 + nota3 + nota4) / 4;
+
+
+// function media(){
+//     document.getElementById('situacao').innerHTML = `A média do aluno foi de: ${total.toFixed(2)}` 
+// }
+
+//resolução do professor Guanabara
