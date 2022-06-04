95cc1425622cc44c75cb13116baa05091f32f5d6
Marcos Paulo Badaró <marcospaulobadaro@gmail.com>
2022-06-03 21:55:08 -0300

    Concluindo o desafio1

---
+++ desafio1/script.js
@@ -0,0 +1,11 @@
+function msg1(){
+    window.alert('Você clicou no primeiro botão!')
+}
+
+function msg2(){
+    window.alert("Você clicou no segundo botão")
+}
+
+function msg3(){
+    window.alert("Você clicou no terceiro botão")
+}
