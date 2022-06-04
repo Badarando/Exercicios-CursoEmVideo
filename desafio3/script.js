ac09e37558df8d80041790c018bd9f027f184394
Marcos Paulo Badaró <marcospaulobadaro@gmail.com>
2022-06-03 22:35:43 -0300

    Desafio3

---
+++ desafio3/script.js
@@ -0,0 +1,6 @@
+function chamada(){
+    let num = Number(window.prompt('Digite um numero inteiro qualquer'));
+    window.alert(
+        `Antes de ${num}, temos ${num - 1}. Depois de ${num}, temos ${num + 1}`)
+
+}
